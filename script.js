694cda97bc78bae76b4a0857fb248818b1d8a546
Gabriella Tavares <gabriellatatavares@gmail.com>
2021-04-15 17:30:20 +0100

    added modal

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const modal = document.querySelector('.modal');
+const previews = document.querySelectorAll('.galleryContainer img');
+const original = document.querySelector('.full-img')
+
+previews.forEach(preview => {
+  preview.addEventListener('click', () => {
+    modal.classList.add("open");
+    original.classList.add('open');
+    const originalSrc = preview.getAttribute("src");
+    original.src = `${originalSrc}` 
+  })
+})
+
+modal.addEventListener('click', (e) => {
+  if(e.target.classList.contains('modal')){
+    modal.classList.remove('open');
+    original.classList.remove('open');
+  }
+})
+
 
 filterSelection("all")
 function filterSelection(c) {
