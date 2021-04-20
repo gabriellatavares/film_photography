6408877b62ebd2140fac95b52900ffe7c73a78f5
Gabriella Tavares <gabriellatatavares@gmail.com>
2021-04-20 21:53:45 +0100

    styled a little bit

--- script.js
+++ script.js
@@ -60,3 +60,24 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+let checkbox = document.querySelector('input[name=theme]')
+
+checkbox.addEventListener('change', function() {
+  if(this.checked){
+    trans()
+    document.documentElement.setAttribute('data-theme', 'dark')
+  } else {
+    trans()
+    document.documentElement.setAttribute('data-theme', 'light')
+
+
+  }
+})
+
+let trans = () => {
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('transition')
+  }, 300)
+}
